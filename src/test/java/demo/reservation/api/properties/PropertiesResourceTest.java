e280cb4bda2b3d05c321fb7138f060e15c988bba
hnasri <hnasri@payplaza.com>
2023-04-11 14:43:58 +0200

    Tests added for better coverage.

--- src/test/java/demo/reservation/api/properties/PropertiesResourceTest.java
+++ src/test/java/demo/reservation/api/properties/PropertiesResourceTest.java
@@ -3,18 +3,46 @@ package demo.reservation.api.properties;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @QuarkusTest
 public class PropertiesResourceTest {
 
+    public static final String GARAGE_WORKING_DAYS = "1,2,3,4,5,6";
+    public static final String MECHANICS_A_WORKING_DAYS = "1,3,4,6";
+    public static final String MECHANICS_B_WORKING_DAYS = "1,2,3,4,5";
+    @Inject
+    PropertiesConfig propertiesConfig;
+
     @Test
-    public void testRootEndpoint() {
-        given()
-            .when().get("/root")
-            .then()
-            .statusCode(200)
-            .body(is("Resources available"));
+    void should_retrieve_garage_working_day() {
+        //check garage working day
+        String garageWorkingDays = propertiesConfig.garageWorkingDays();
+        assertEquals(GARAGE_WORKING_DAYS, garageWorkingDays);
+    }
+
+    @Test
+    void should_not_include_sundays_in_working_days() {
+        //check garage working day
+        String garageWorkingDays = propertiesConfig.garageWorkingDays();
+        assertFalse(garageWorkingDays.contains("7"));
+
+    }
+
+    @Test
+    void should_validate_mechanicsA_working_days() {
+        //check mechanics A working day
+        String mechanicsAWorkingDays = propertiesConfig.mechanicsAWorkingDays();
+        assertEquals(MECHANICS_A_WORKING_DAYS, mechanicsAWorkingDays);
+    }
+
+    @Test
+    void should_validate_mechanicsB_working_days() {
+        //check mechanics A working day
+        String mechanicsBWorkingDays = propertiesConfig.mechanicsBWorkingDays();
+        assertEquals(MECHANICS_B_WORKING_DAYS, mechanicsBWorkingDays);
     }
 }
